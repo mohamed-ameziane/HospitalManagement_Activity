14badcc06594a3170ecb51f165ec78c578e4111f
Mohamed Ameziane <med.ameez02@gmail.com>
2025-03-17 10:14:21 +0000

    Update main code

--- src/main/java/ma/enset/hospital_management/HospitalManagementActivityApplication.java
+++ src/main/java/ma/enset/hospital_management/HospitalManagementActivityApplication.java
@@ -1,13 +1,47 @@
 package ma.enset.hospital_management;
 
+import ma.enset.hospital_management.entities.Patient;
+import ma.enset.hospital_management.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
-public class HospitalManagementActivityApplication {
+public class HospitalManagementActivityApplication implements CommandLineRunner {
+
+    @Autowired
+    private PatientRepository patientRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(HospitalManagementActivityApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Patient patient = new Patient();
+        patient.setId(null);
+        patient.setNom("Amine");
+        patient.setDateNaissance(new Date());
+        patient.setMalade(true);
+        patient.setScore(10);
+
+        /* En utilisant le constructeur avec params
+        Patient patient = new Patient(null, "Amine", new Date(), true, 10);
+        */
+
+        /* En utilisant Builder
+        Patient patient = Patient.builder()
+                .nom("Amine")
+                .dateNaissance(new Date())
+                .isMalade(true)
+                .score(10)
+                .build();
+        */
+
+        patientRepository.save(patient);
+
+    }
 }
