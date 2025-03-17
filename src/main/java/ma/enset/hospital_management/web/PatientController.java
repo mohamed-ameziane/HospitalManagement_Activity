c6da415a9d2883125d03218c293549343a963b29
Mohamed Ameziane <med.ameez02@gmail.com>
2025-03-17 11:14:08 +0000

    Add pagination feature

--- src/main/java/ma/enset/hospital_management/web/PatientController.java
+++ src/main/java/ma/enset/hospital_management/web/PatientController.java
@@ -3,9 +3,12 @@ package ma.enset.hospital_management.web;
 import lombok.AllArgsConstructor;
 import ma.enset.hospital_management.entities.Patient;
 import ma.enset.hospital_management.repository.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,10 +18,14 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping("/index")
-    public String index(Model model){
-        List<Patient> listPatients = patientRepository.findAll();
-        model.addAttribute("patients", listPatients);
+    public String index(Model model, @RequestParam(name="page", defaultValue = "0") int page, @RequestParam(name="size", defaultValue = "4") int size){
+//        List<Patient> listPatients = patientRepository.findAll();
+//        model.addAttribute("patients", listPatients);
 
+        Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page, size));
+        model.addAttribute("patients", pagePatients);
+        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage", page);
         return "patients";
     }
 
