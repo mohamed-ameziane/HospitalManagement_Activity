a8e6488297a56b555b5cffc85bb4ad7ff313aa4c
Mohamed Ameziane <med.ameez02@gmail.com>
2025-03-17 10:06:37 +0000

    Project initialization

---
+++ src/test/java/ma/enset/hospital_management/HospitalManagementActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.hospital_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementActivityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
